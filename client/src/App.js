e100ba280a89d3415dada76f7078c9b576467688
Bbold72 <reinbold.brian.j@gmail.com>
2022-09-06 15:05:42 -0500

    add title to home page

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
           >
             Learn React Here
           </a>
+          <h1 className='App-title'>Fib Calculator Version K8s</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
